640c3dab5fd28f615ef1b6ab12e098dae4c1291a
Antoine Roucau <antoine.roucau@gmail.com>
2024-12-10 18:07:39 +0100

    a

--- main.py
+++ main.py
@@ -14,46 +14,6 @@ class Plateau:
             rep += str(ligne) + "\n"
         return rep
 
-
-class Pos(Plateau):
-    def __init__(self, x, y, plato=None):
-        Plateau.__init__(self, plato)
-        if self.plato[x,y] == 0:
-            self.x = x
-            self.y = y
-        else:
-            print("Erreur de coordonnées")
-
-    def __str__(self):
-        return f"x = {self.x} et  y = {self.y}"
-
-
-def let(joueur):
-    if joueur == 0:
-        return 'X'
-    elif joueur == 1:
-        return 'O'
-    else:
-        return 'Joueur invalide'
-
-
-def coup(pos, joueur):
-    pos.plato[pos.x,pos.y] = let(joueur)
-    return pos.plato
-
-
-def gagne(plato):
-    x = True
-
-    return x
-
 plat=Plateau()
-pos1=Pos(1,1)
-print(pos1)
+print(plat)
 
-print("boubouboubou")
-print("En s'en fout")
-
-
-print("les beaux")
-print("jacass")
