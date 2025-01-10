f22f9c475aa87f9799814c5384cea181603f33d5
Antoine Roucau <antoine.roucau@gmail.com>
2025-01-10 13:33:46 +0100

    et non

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ class Game:
                         'trois_ouvert': {'seq': [0,1,1,1,0], 'score': 1000},
                         'trois_gauche': {'seq': [0,1,1,1], 'score': 100},  
                         'trois_droite': {'seq': [1,1,1,0], 'score': 100},
-                        'trois_semi': {'seq': [1,0,1], 'score': 50},
                         'deux_ouvert': {'seq': [0,1,1,0], 'score': 10} 
 
         }
